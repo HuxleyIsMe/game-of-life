3d637d900d4a157aa8776fd80746c2beb3aa1845
Huxley <huxley@Huxleys-MBP.fritz.box>
2022-11-01 18:25:43 +0100

    have a grid and indexs

--- src/App.js
+++ src/App.js
@@ -1,23 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
+const CUBE_OF_TILES = 10;
 function App() {
+  const [tileMatrix, setTileMatrix] = React.useState(
+    new Array(CUBE_OF_TILES)
+      .fill("")
+      .map(() => new Array(CUBE_OF_TILES).fill(0))
+  );
+  const [initialTiles, setInitialTiles] = React.useState([
+    { x: 3, y: 3 },
+    { x: 4, y: 3 },
+    { x: 2, y: 3 },
+  ]);
+
+  const isAlive = (coords) => !!tileMatrix[coords.y][coords.x];
+
+  React.useEffect(() => {
+    let NextTileMatrix = [...tileMatrix];
+    initialTiles.forEach((tile) => (NextTileMatrix[tile.y][tile.x] = 1));
+    setTileMatrix(NextTileMatrix);
+  }, []);
+
+  React.useEffect(() => {
+    console.log("hi");
+  }, [tileMatrix]);
+
+  const TILE_GRID = React.useMemo(() => {
+    const generateRow = (yPosition) =>
+      new Array(CUBE_OF_TILES).fill("").map((__, index) => (
+        <div
+          className={`Tile ${
+            isAlive({ x: index, y: yPosition }) ? "Alive" : ""
+          }`}
+          key={`${index}-${yPosition}`}
+        >
+          {index}
+        </div>
+      ));
+
+    return (
+      <div className="TileGrid">
+        {new Array(CUBE_OF_TILES).fill("").map((__, index) => (
+          <div className="row" key={`row-${index}`}>
+            {generateRow(index)}
+          </div>
+        ))}
+      </div>
+    );
+  }, [tileMatrix]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <h1>tile clicked {JSON.stringify(initialTiles)}</h1>
+        <label htmlFor="intialStart">Intial starting cells</label>
+        <input></input>
+      </div>
+
+      {TILE_GRID}
     </div>
   );
 }
